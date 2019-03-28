3cb79e621ef9982d039d3770fbe02a0bed208481
Guobiao Mo <guobiaomo@chinamobile.com>
2019-03-28 14:37:41 -0700

    Unit test for seed code
    This project depends heavily on outside
    infrastructure, thus there are not much
    unit test cases.
    
    Issue-ID: DCAEGEN2-1309
    Change-Id: I8893f4029014673139e67bf373fd77e9e5c42f83
    Signed-off-by: Guobiao Mo <guobiaomo@chinamobile.com>

---
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/config/ApplicationConfigurationTest.java
@@ -0,0 +1,73 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : DATALAKE
+* ================================================================================
+* Copyright 2019 China Mobile
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+
+package org.onap.datalake.feeder.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.datalake.feeder.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * test ApplicationConfiguration
+ * 
+ * @author Guobiao Mo
+ *
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+/*
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class, 
+                      initializers = ConfigFileApplicationContextInitializer.class)
+*/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ApplicationConfiguration.class)
+//@ActiveProfiles("test")
+public class ApplicationConfigurationTest {
+
+	@Autowired
+	private ApplicationConfiguration config;
+
+	@Test
+	public void readConfig() {
+		assertNotNull(config.getCouchbaseHost());
+		assertNotNull(config.getCouchbaseUser());
+		assertNotNull(config.getCouchbasePass());
+		assertNotNull(config.getCouchbaseBucket());
+
+		assertNotNull(config.getDmaapZookeeperHostPort());
+		assertNotNull(config.getDmaapKafkaHostPort());
+		assertNotNull(config.getDmaapKafkaGroup());
+		assertTrue(config.getDmaapKafkaTimeout() > 0L);
+		assertTrue(config.getDmaapCheckNewTopicIntervalInSec() > 0);
+
+		assertTrue(config.getKafkaConsumerCount() > 0);
+	}
+
+}
