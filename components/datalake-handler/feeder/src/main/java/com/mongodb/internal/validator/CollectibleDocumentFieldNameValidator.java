f37bd85719480003c71d61f549241aeff98a891f
Guobiao Mo <guobiaomo@chinamobile.com>
2019-04-16 23:54:45 -0700

    Unit test to improve sonar coverage
    Issue-ID: DCAEGEN2-1309
    Change-Id: Ia3651bb105d92013d6d2a9d60fffe3b6066adf64
    Signed-off-by: Guobiao Mo <guobiaomo@chinamobile.com>

--- components/datalake-handler/feeder/src/main/java/com/mongodb/internal/validator/CollectibleDocumentFieldNameValidator.java
+++ components/datalake-handler/feeder/src/main/java/com/mongodb/internal/validator/CollectibleDocumentFieldNameValidator.java
@@ -8,7 +8,7 @@ package com.mongodb.internal.validator;
 * ================================================================================
 * Copyright 2018 China Mobile
 *=================================================================================
-* Licensed under the Apache License, Version 2.0 (the "License");
+* Licensed under the Apache License, Version 2.0 (the "License")
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
