dd2e6ccc8e1922dd4e54f67f15d5e5f50a16e0c7
ZhangZihao <zhangzihao@chinamobile.com>
2019-04-22 09:54:44 +0800

    ‘correlateClearedMessage’
    Change-Id: I52dca705376d9d62e6d7c18a7843a2c3d85dfb3a
    Issue-ID: DCAEGEN2-1190
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

--- components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/service/ElasticsearchService.java
+++ components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/service/ElasticsearchService.java
@@ -22,12 +22,22 @@ package org.onap.datalake.feeder.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.replication.ReplicationResponse;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest; 
@@ -38,6 +48,11 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.json.JSONObject;
 import org.onap.datalake.feeder.config.ApplicationConfiguration;
 import org.onap.datalake.feeder.domain.Db;
@@ -67,6 +82,8 @@ public class ElasticsearchService {
 
 	private RestHighLevelClient client;
 	ActionListener<BulkResponse> listener;
+	ActionListener<GetResponse> getListener;
+	ActionListener<IndexResponse> indexListener;
 
 
 //ES Encrypted communication https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/_encrypted_communication.html#_encrypted_communication
@@ -118,7 +135,7 @@ public class ElasticsearchService {
 
 		for (JSONObject json : jsons) {
 			if(topic.isCorrelateClearedMessage()) {
-				boolean found = correlateClearedMessage(json);
+				boolean found = correlateClearedMessage(topic, json);
 				if(found) {
 					continue;
 				}
@@ -138,21 +155,145 @@ public class ElasticsearchService {
 			}
 		}
 	}
-	
-	private boolean correlateClearedMessage(JSONObject json) {
-		boolean found = true;
-				
-		/*TODO
-		 * 1. check if this is a alarm cleared message
-		 * 2. search previous alarm message
-		 * 3. update previous message, if success, set found=true
-		 */
-		//for Sonar test, remove the following
-		if(json.isNull("kkkkk")) {
-			found = false;
+
+
+
+
+	/**
+	 *
+	 * @param topic
+	 * @param json
+	 * @return
+	 *
+	 * Because of query by id, The search API cannot be used for query.
+	 * The search API can only query all data or based on the fields in the source.
+	 * So use the get API, three parameters: index, type, document id
+	 */
+	public boolean correlateClearedMessage(Topic topic, JSONObject json) {
+		boolean found = false;
+		String eName = null;
+
+		try {
+			eName = json.getJSONObject("event").getJSONObject("commonEventHeader").getString("eventName");
+
+			if (StringUtils.isNotBlank(eName)) {
+
+				if (eName.endsWith("Cleared")) {
+
+					String name = eName.replace("Cleared","");
+					String reportingEntityName = json.getJSONObject("event").getJSONObject("commonEventHeader").getString("reportingEntityName");
+					String specificProblem = json.getJSONObject("event").getJSONObject("faultFields").getString("specificProblem");
+
+					String id = null;
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder = stringBuilder.append(name).append('^').append(reportingEntityName).append('^').append(specificProblem);
+
+					id = stringBuilder.toString();//example: id = "aaaa^cccc^bbbbb"
+					String index = topic.getName().toLowerCase();
+
+					//get
+					//index: topic.getName().toLowerCase(), document id: id
+					GetRequest getRequest = new GetRequest(index, "doc", id);
+
+					getListener = new ActionListener<GetResponse>() {
+						@Override
+						public void onResponse(GetResponse getResponse) {
+							//async success
+							successResponse(getResponse);
+
+						}
+
+						@Override
+						public void onFailure(Exception e) {
+
+						}
+					};
+
+					if(config.isAsync()) {
+						//async
+						client.getAsync(getRequest, RequestOptions.DEFAULT, getListener);
+					}else {
+						//sync
+						GetResponse getResponse = null;
+						try {
+							getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+							successResponse(getResponse);
+						} catch (ElasticsearchException e) {
+							if (e.status() == RestStatus.NOT_FOUND) {
+								log.error("The document for this id was not found" );
+							}
+							if (e.status() == RestStatus.CONFLICT) {
+								log.error("Version conflict" );
+							}
+							log.error("Get document exception", e);
+						}catch (IOException e) {
+							log.error(topic.getName() , e);
+						}
+					}
+					found = true;
+				} else {
+					log.info("The data is normal");
+				}
+
+			} else {
+				log.debug("event id null, do not store");
+				found = true;
+			}
+
+		} catch (Exception e) {
+			//e.printStackTrace();
+			log.error("error",e);
 		}
 		
 		return found; 
+	}
+
+	private void successResponse(GetResponse getResponse) {
+
+		if(getResponse != null) {
+			String index = getResponse.getIndex();
+			String type = getResponse.getType();
+			String id = getResponse.getId();
+			if (getResponse.isExists()) {
+				long version = getResponse.getVersion();
+				String sourceAsString = getResponse.getSourceAsString();
+				JSONObject jsonObject = new JSONObject(sourceAsString);
+				jsonObject.getJSONObject("event").getJSONObject("faultFields").put("vfStatus","close");
+				String jsonString = jsonObject.toString();
+				//update
+				saveJson(index, type, id, jsonString);
+
+			} else {
+				log.error("The document for this id was not found" );
+			}
+		}
+	}
+
+	private void saveJson(String index, String type, String id, String jsonString) {
+		IndexRequest request = new IndexRequest(index, type, id);
+
+		request.source(jsonString, XContentType.JSON);
+		indexListener = new ActionListener<IndexResponse>() {
+			@Override
+			public void onResponse(IndexResponse indexResponse) {
+
+			}
+
+			@Override
+			public void onFailure(Exception e) {
+
+			}
+		};
+
+		if(config.isAsync()) {
+			client.indexAsync(request, RequestOptions.DEFAULT, indexListener);
+		}else {
+			try {
+				client.index(request, RequestOptions.DEFAULT);
+			} catch (IOException e) {
+				log.error("save failure");
+			}
+		}
 	}
 
 }
