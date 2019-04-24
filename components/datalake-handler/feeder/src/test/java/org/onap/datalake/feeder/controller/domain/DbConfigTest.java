63b3725490d9505154a2c44e30e0c02414a0fc02
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2019-04-24 15:55:57 +0530

    Unit test code for datalake seed code
    Increased the code coverage to 100% for
    domain controller module
    
    Issue-ID: DCAEGEN2-1309
    
    Change-Id: Ie0d737323928c6350e8d41ada2eb3880975a93bc
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

---
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/controller/domain/DbConfigTest.java
@@ -0,0 +1,47 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : DATALAKE
+ * ================================================================================
+ * Copyright (C) 2018-2019 Huawei. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.feeder.controller.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DbConfigTest {
+    @Test
+    public void testDbConfig() {
+        DbConfig dbConfig = new DbConfig();
+        dbConfig.setName("elasticsearch");
+        assertTrue("elasticsearch".equals(dbConfig.getName()));
+        dbConfig.setHost("localhost");
+        assertTrue("localhost".equals(dbConfig.getHost()));
+        dbConfig.setLogin("root");
+        assertTrue("root".equals(dbConfig.getLogin()));
+        dbConfig.setPassword("root123");
+        assertTrue("root123".equals(dbConfig.getPassword()));
+        dbConfig.setDatabase("elasticsearch");
+        assertTrue("elasticsearch".equals(dbConfig.getDatabase()));
+        dbConfig.setPort(123);
+        assertEquals(123, dbConfig.getPort());
+        dbConfig.setPoperties("driver");
+        assertTrue("driver".equals(dbConfig.getPoperties()));
+    }
+}
