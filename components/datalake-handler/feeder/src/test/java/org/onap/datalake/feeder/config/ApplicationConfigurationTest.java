36071d7d192ddb33bddac3dcf4dff26ad0ad96c6
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2019-04-26 14:21:41 +0530

    Unit test code for datalake seed code
    Improved Application Configuration code coverage to 100%
    
    Issue-ID: DCAEGEN2-1309
    
    Change-Id: Ic42de3b39ba8468fa0c0749700a852ec0162e52b
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/config/ApplicationConfigurationTest.java
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/config/ApplicationConfigurationTest.java
@@ -26,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -64,6 +65,7 @@ public class ApplicationConfigurationTest {
         assertNotNull(config.getDefaultTopicName());
         assertNotNull(config.getRawDataLabel());
         assertNotNull(config.getTimestampLabel());
+        assertEquals(null, config.getElasticsearchType());
     }
 
 }
