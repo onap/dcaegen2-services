31ac8fe1aed56d5848bdeeea194fb0f50e3343ff
ZhangZihao <zhangzihao@chinamobile.com>
2019-04-26 10:03:05 +0800

    unit test of service
    Change-Id: Ie6a7838e242d96543c95f101e06e1b8c9e8d0099
    Issue-ID: DCAEGEN2-1309
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

---
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/service/DmaapServiceTest.java
@@ -0,0 +1,80 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : DATALAKE
+ * ================================================================================
+ * Copyright 2019 China Mobile
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.feeder.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.datalake.feeder.config.ApplicationConfiguration;
+import org.onap.datalake.feeder.domain.Topic;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DmaapServiceTest {
+
+    static String DMAPP_ZOOKEEPER_HOST_PORT = "message-router-zookeeper:2181";
+
+    @InjectMocks
+    private DmaapService dmaapService;
+
+    @Mock
+    private ApplicationConfiguration config;
+    @Mock
+    private TopicService topicService;
+
+    @Test
+    public void testGetTopics() {
+
+        List<String> list = new ArrayList<>();
+        list.add("unauthenticated.DCAE_CL_OUTPUT");
+        list.add("AAI-EVENT");
+        list.add("__consumer_offsets");
+        list.add("unauthenticated.SEC_FAULT_OUTPUT");
+        list.add("msgrtr.apinode.metrics.dmaap");
+        when(config.getDmaapZookeeperHostPort()).thenReturn(DMAPP_ZOOKEEPER_HOST_PORT);
+        assertEquals(dmaapService.getTopics(), list);
+    }
+
+    @Test
+    public void testGetActiveTopics() throws IOException {
+
+        List<String> list = new ArrayList<>();
+        list.add("unauthenticated.DCAE_CL_OUTPUT");
+        list.add("AAI-EVENT");
+        list.add("__consumer_offsets");
+        list.add("unauthenticated.SEC_FAULT_OUTPUT");
+        list.add("msgrtr.apinode.metrics.dmaap");
+
+        when(config.getDmaapZookeeperHostPort()).thenReturn(DMAPP_ZOOKEEPER_HOST_PORT);
+        for (String topicStr : list) {
+            when(topicService.getEffectiveTopic(topicStr, true)).thenReturn(new Topic());
+        }
+        assertEquals(dmaapService.getActiveTopics(),new ArrayList<>());
+    }
+}
