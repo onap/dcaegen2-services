8a293ec9a1cc40ec1c18f1185c1761b54225b898
ZhangZihao <zhangzihao@chinamobile.com>
2019-05-31 10:55:37 +0800

    Dashboard and esTemplate
    Change-Id: Id87cb7846ac50ab5e0deee9582479c41aa4e5484
    Issue-ID: DCAEGEN2-1329
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

---
+++ components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/repository/PortalRepository.java
@@ -0,0 +1,42 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : DataLake
+ * ================================================================================
+ * Copyright 2019 China Mobile
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.feeder.repository;
+
+import org.onap.datalake.feeder.domain.Portal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+/**
+ * Portal Repository
+ *
+ * @author guochunmeng
+ */
+
+public interface PortalRepository extends CrudRepository<Portal, String> {
+
+    List<Portal> findByEnabled(Boolean enabled);
+
+}
---
+++ components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/service/PortalService.java
@@ -0,0 +1,58 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : DataLake
+ * ================================================================================
+ * Copyright 2019 China Mobile
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.feeder.service;
+
+
+import org.onap.datalake.feeder.domain.Portal;
+import org.onap.datalake.feeder.dto.PortalConfig;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for portals
+ *
+ * @author guochunmeng
+ *
+ */
+@Service
+public class PortalService {
+
+    public Portal fillPortalConfiguration(PortalConfig portalConfig)
+    {
+        Portal portal = new Portal();
+        fillPortal(portalConfig, portal);
+        return portal;
+    }
+    public void fillPortalConfiguration(PortalConfig portalConfig, Portal portal)
+    {
+        fillPortal(portalConfig, portal);
+    }
+
+    private void fillPortal(PortalConfig portalConfig, Portal portal) {
+
+        portal.setName(portalConfig.getName());
+        portal.setLogin(portalConfig.getLogin());
+        portal.setPass(portalConfig.getPass());
+        portal.setEnabled(portalConfig.getEnabled());
+        portal.setHost(portalConfig.getHost());
+        portal.setPort(portalConfig.getPort());
+
+    }
+}
