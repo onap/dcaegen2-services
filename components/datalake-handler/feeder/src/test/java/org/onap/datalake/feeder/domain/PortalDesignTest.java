1022fab37e0ad9696d8217f363a5aaa86bb16fa8
ZhangZihao <zhangzihao@chinamobile.com>
2019-06-11 14:32:33 +0800

    Unit test 1
    Change-Id: I44ad9a41a35a056a9b8bc9d6c760acb5e5413b80
    Issue-ID: DCAEGEN2-1468
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

---
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/domain/PortalDesignTest.java
@@ -0,0 +1,52 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : DATALAKE
+ * ================================================================================
+ * Copyright 2019 China Mobile
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.feeder.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PortalDesignTest {
+
+    @Test
+    public void testIs() {
+
+        PortalDesign portalDesign = new PortalDesign();
+        portalDesign.setId(1);
+        portalDesign.setSubmitted(false);
+        portalDesign.setBody("jsonString");
+        portalDesign.setName("templateTest");
+        Topic topic = new Topic("_DL_DEFAULT_");
+        portalDesign.setTopic(topic);
+        DesignType designType = new DesignType();
+        designType.setName("Kibana");
+        portalDesign.setDesignType(designType);
+        portalDesign.setNote("test");
+        assertFalse("1".equals(portalDesign.getId()));
+        assertTrue("templateTest".equals(portalDesign.getName()));
+        assertTrue("jsonString".equals(portalDesign.getBody()));
+        assertFalse("_DL_DEFAULT_".equals(portalDesign.getTopic()));
+        assertTrue("test".equals(portalDesign.getNote()));
+        assertFalse("Kibana".equals(portalDesign.getDesignType()));
+        assertFalse("false".equals(portalDesign.getSubmitted()));
+    }
+
+}
