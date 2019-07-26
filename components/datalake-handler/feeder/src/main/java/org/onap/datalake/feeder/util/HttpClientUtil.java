d935d95b9fd215457e3c0a65b98e7265f187137e
ZhangZihao <zhangzihao@chinamobile.com>
2019-07-26 11:33:06 +0800

    deploy improve 1
    Change-Id: I9ba21e0e0f8e6ab75d63e65457bc382aa02cff91
    Issue-ID: DCAEGEN2-1658
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

--- components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/util/HttpClientUtil.java
+++ components/datalake-handler/feeder/src/main/java/org/onap/datalake/feeder/util/HttpClientUtil.java
@@ -21,14 +21,14 @@
 package org.onap.datalake.feeder.util;
 
 import com.google.gson.Gson;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -42,33 +42,31 @@ import java.util.Map;
  */
 public class HttpClientUtil {
 
-    private final static String KIBANA_DASHBOARD_IMPORT = "KibanaDashboardImport";
+    private static final Logger log = LoggerFactory.getLogger(HttpClientUtil.class);
 
-    private final static String ELASTICSEARCH_MAPPING_TEMPLATE = "ElasticsearchMappingTemplate";
+    private static final String KIBANA_DASHBOARD_IMPORT = "KibanaDashboardImport";
 
-    public static boolean sendPostHttpClient(String url, String json, String postFlag){
+    private static final String ELASTICSEARCH_MAPPING_TEMPLATE = "ElasticsearchMappingTemplate";
 
-        boolean flag = true;
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(url);
-        String response = null;
+    private HttpClientUtil() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static boolean sendPostHttpClient(String url, String json, String postFlag) {
+        boolean flag = false;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("kbn-xsrf","true");
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<String> responseEntity = null;
         try {
-            StringEntity s = new StringEntity(json);
-            s.setContentEncoding("UTF-8");
-            s.setContentType("application/json");
-            post.setEntity(s);
-            post.setHeader("kbn-xsrf","true");
-            post.setHeader("Accept", "*/*");
-            post.setHeader("Connection", "Keep-Alive");
-            HttpResponse res = client.execute(post);
-            if(res.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-                HttpEntity entity = res.getEntity();
-                String result = EntityUtils.toString(res.getEntity());
-                response = result;
-            }
+            responseEntity = restTemplate.postForEntity(url, request, String.class);
+            if (responseEntity.getStatusCodeValue() != 200)
+                throw new RestClientException("Resquest failed");
             Gson gson = new Gson();
             Map<String, Object> map = new HashMap<>();
-            map = gson.fromJson(response, map.getClass());
+            map = gson.fromJson(responseEntity.getBody(), map.getClass());
             switch (postFlag) {
                 case KIBANA_DASHBOARD_IMPORT:
                     flag = flagOfKibanaDashboardImport(map);
@@ -76,21 +74,22 @@ public class HttpClientUtil {
                 case ELASTICSEARCH_MAPPING_TEMPLATE :
                     flag = flagOfPostEsMappingTemplate(map);
                     break;
+                default:
+                    break;
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.debug(e.getMessage());
         }
         return flag;
     }
-
 
     private static boolean flagOfKibanaDashboardImport(Map<String, Object> map) {
 
         boolean flag = true;
         List objectsList = (List) map.get("objects");
 
-        if (objectsList != null && objectsList.size() > 0) {
-            Map<String, Object> map2 = new HashMap<>();
+        if (!objectsList.isEmpty()) {
+            Map<String, Object> map2 = null;
             for (int i = 0; i < objectsList.size(); i++){
                 map2 = (Map<String, Object>)objectsList.get(i);
                 for(String key : map2.keySet()){
@@ -103,12 +102,11 @@ public class HttpClientUtil {
         return flag;
     }
 
-
     private static boolean flagOfPostEsMappingTemplate(Map<String, Object> map) {
 
         boolean flag = true;
         for(String key : map.keySet()){
-            if ("acknowledged".equals(key) && (boolean) map.get("acknowledged") == true) {
+            if ("acknowledged".equals(key) && (boolean) map.get("acknowledged")) {
                 break;
             } else {
                 flag = false;
@@ -116,5 +114,4 @@ public class HttpClientUtil {
         }
         return flag;
     }
-
 }
