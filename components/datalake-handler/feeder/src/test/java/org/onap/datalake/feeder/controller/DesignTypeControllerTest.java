279f6cf543a6d28204b9ab954a49eee3ca211642
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2019-08-07 18:55:52 +0530

    Removed unused imports
    Also some places were using import with * option
    
    Issue-ID: DCAEGEN2-1468
    
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>
    Change-Id: I0f57bbb429816d9aa6219a4a1e4fc4c384bec873

--- components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/controller/DesignTypeControllerTest.java
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/controller/DesignTypeControllerTest.java
@@ -33,7 +33,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DesignTypeControllerTest {
