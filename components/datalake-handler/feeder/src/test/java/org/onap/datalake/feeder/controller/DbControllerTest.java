01ac9eeada4914d29066711eccbf00f691a35f01
ZhangZihao <zhangzihao@chinamobile.com>
2019-10-23 11:47:44 +0800

    dbs modify  code
    Change-Id: I87f0a339382354fa9ca6cbded9d1403a234f39f4
    Issue-ID: DCAEGEN2-1622
    Signed-off-by: ZhangZihao <zhangzihao@chinamobile.com>

--- components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/controller/DbControllerTest.java
+++ components/datalake-handler/feeder/src/test/java/org/onap/datalake/feeder/controller/DbControllerTest.java
@@ -42,7 +42,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.Collections;
 
@@ -72,7 +71,7 @@ public class DbControllerTest {
         dbConfig.setName("Elecsticsearch");
         dbConfig.setHost("localhost");
         dbConfig.setLogin("root");
-        dbConfig.setPassword("root123");
+        dbConfig.setPass("root123");
         dbConfig.setDatabase("Elecsticsearch");
         dbConfig.setPort(123);
         dbConfig.setPoperties("driver");
@@ -139,7 +138,7 @@ public class DbControllerTest {
         for (String dbName : list) {
             assertEquals("Elecsticsearch", dbName);
         }
-        dbController.deleteDb("Elecsticsearch", httpServletResponse);
+        //dbController.deleteDb("Elecsticsearch", httpServletResponse);
     }
 
 
@@ -165,7 +164,7 @@ public class DbControllerTest {
         	tmp.setId(2);
             assertNotEquals(tmp, anElecsticsearch);
         }
-        dbController.deleteDb(dbName, httpServletResponse);
+        //dbController.deleteDb(dbName, httpServletResponse);
     }
 
     @Test
