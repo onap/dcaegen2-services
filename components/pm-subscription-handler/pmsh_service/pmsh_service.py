f49d5a4ee01f6229bea890bf54d6c9ef2a490f60
efiacor <fiachra.corcoran@est.tech>
2019-11-06 11:08:54 +0000

    Initial commit for PMSH
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Issue-ID: DCAEGEN2-1837
    Change-Id: I3b7d7d379df68f0f5984a3c4e0faa9c8ca0bcdf8

---
+++ components/pm-subscription-handler/pmsh_service/pmsh_service.py
@@ -0,0 +1,36 @@
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2019 Nordix Foundation.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+import os
+import time
+
+import mod.pmsh_logging as logger
+from mod.dmaap_client import dummy_dmaap_function
+
+
+def main():
+    """Entrypoint"""
+    if "PROD_LOGGING" in os.environ:
+        logger.create_loggers()
+    while True:
+        time.sleep(30)
+        dummy_dmaap_function()
+
+
+if __name__ == '__main__':
+    main()
