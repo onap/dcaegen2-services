3d111bf4267cafc5b4dbb8cde8380e29cb6b314c
efiacor <fiachra.corcoran@est.tech>
2019-12-11 12:00:26 +0000

    Adding AAI client and apply filter
    	# Adding Subscription class
    	# Adding MR and Utils helper classes
    
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I791b506d8cf166737d67cef22052852256f8a811
    Issue-ID: DCAEGEN2-1930

--- components/pm-subscription-handler/tests/test_subscription.py
+++ components/pm-subscription-handler/tests/test_subscription.py
@@ -15,14 +15,28 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # ============LICENSE_END=====================================================
-
+import json
+import os
 import unittest
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+from subscription import Subscription
 
 
-if __name__ == '__main__':
-    unittest.main()
+class SubscriptionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        with open(os.path.join(os.path.dirname(__file__), 'data/cbs_data.json'), 'r') as data:
+            self.cbs_data = json.load(data)
+        self.sub = Subscription(**self.cbs_data['policy']['subscription'])
+
+    def test_sub_filter_true(self):
+        self.assertTrue(self.sub.apply_filter_to_xnf('pnf1'))
+
+    def test_sub_filter_false(self):
+        self.assertFalse(self.sub.apply_filter_to_xnf('PNF-33'))
+
+    def test_sub_measurement_group(self):
+        self.assertEqual(len(self.sub.measurementGroups), 2)
+
+    def test_sub_file_location(self):
+        self.assertEqual(self.sub.fileLocation, '/pm/pm.xml')
