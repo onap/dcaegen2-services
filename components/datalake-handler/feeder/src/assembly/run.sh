3034e7cd569faea033c9a309b3318c7e51c1ba9e
Kate Hsuan <kate.hsuan@qct.io>
2020-03-02 08:54:04 +0000

    Switch from cloudify helm plugin to cloudify/tosca.
    1. Blueprint implementation.
    2. Modify startup sequence.
    
    Issue-ID: DCAEGEN2-2098
    Signed-off-by: Kate Hsuan <kate.hsuan@qct.io>
    Change-Id: I9d3d56565b0e997d3150eeef0891cc79bb0519ae

--- components/datalake-handler/feeder/src/assembly/run.sh
+++ components/datalake-handler/feeder/src/assembly/run.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-java -jar feeder-1.0.0-SNAPSHOT.jar
+echo "start init db ..."
+
+/bin/run-parts /home/datalake/db_init
+
+echo "finish init db"
+
+cmd=`ls feeder-*.jar`
+if [ -z "$cmd" ]; then
+    echo "STRING is empty"
+    sleep 10000
+else
+    java -jar $cmd
+fi
