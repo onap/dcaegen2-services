eaf19028079ed40f0dd3b9102940f4b8b223f069
Reshmasree <reshmasree.chamarthi2@wipro.com>
2020-08-03 16:08:41 +0530

    Add seed code for slice-analysis-ms
    Issue-ID: DCAEGEN2-2376
    Signed-off-by: Reshmasree <reshmasree.chamarthi2@wipro.com>
    Change-Id: I946ebb346f8a77c22aa4c7590a077c8b3b7d1aa2

---
+++ components/slice-analysis-ms/README.md
@@ -0,0 +1,39 @@
+SLICE-ANALYSIS-MS 
+
+### Build Instructions
+
+This project is organized as a mvn project and is a sub-project of dcaegen2/services (inside components directory). The build generate a jar and package into docker container. 
+
+```
+git clone https://gerrit.onap.org/r/dcaegen2/services
+To build slice-analysis-ms run `mvn clean install` from **components/slice-analysis-ms** directory
+To build docker image run `mvn clean install docker:build`
+```
+
+
+### Environment variables in Docker Container
+
+
+Variables coming from deployment system:
+
+- APP_NAME - slice-analysis-ms application name that will be registered with consul
+- CONSUL_PROTOCOL - Consul protocol by default set to **http**, if it is need to change it then that can be set to different value 
+- CONSUL_HOST - used with conjunction with CBSPOLLTIMER, should be a host address (without port! e.g my-ip-or-host) where Consul service lies
+- CBS_PROTOCOL - Config Binding Service protocol by default set to **http**, if it is need to change it then that can be set to different value
+- CONFIG_BINDING_SERVICE - used with conjunction with CBSPOLLTIMER, should be a name of CBS as it is registered in Consul
+- HOSTNAME - used with conjunction with CBSPOLLTIMER, should be a name of slice-analysis-ms application as it is registered in CBS catalog
+
+
+### Deployment
+
+
+### Standalone deployment
+Slice analysis ms can be deployed standalone using docker-compose.
+
+Navigate to src/main/docker directory. docker-compose.yaml can be found there.
+
+To install :
+    docker-compose up
+
+To uninstall :
+    docker-compose down
---
+++ components/slice-analysis-ms/src/main/java/org/onap/slice/analysis/ms/dmaap/PolicyDmaapClient.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ *  ============LICENSE_START=======================================================
+ *  slice-analysis-ms
+ *  ================================================================================
+ *   Copyright (C) 2020 Wipro Limited.
+ *   ==============================================================================
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *     ============LICENSE_END=========================================================
+ *
+ *******************************************************************************/
+
+package org.onap.slice.analysis.ms.dmaap;
+
+import com.att.nsa.cambria.client.CambriaBatchingPublisher;
+import java.io.IOException;
+import java.util.Map;
+
+import org.onap.slice.analysis.ms.beans.Configuration;
+import org.onap.slice.analysis.ms.utils.DmaapUtils;
+
+public class PolicyDmaapClient {
+
+    private DmaapUtils dmaapUtils;
+
+    private Configuration configuration;
+
+    public PolicyDmaapClient(DmaapUtils dmaapUtils, Configuration configuration) {
+        this.dmaapUtils = dmaapUtils;
+        this.configuration = configuration;
+    }
+
+    /**
+     * Method stub for sending notification to policy.
+     */
+    @SuppressWarnings("unchecked")
+    public boolean sendNotificationToPolicy(String msg) {
+        Map<String, Object> streamsPublishes = configuration.getStreamsPublishes();
+        String policyTopicUrl = ((Map<String, String>) ((Map<String, Object>) streamsPublishes.get("CL_topic"))
+                .get("dmaap_info")).get("topic_url");
+        String[] policyTopicSplit = policyTopicUrl.split("\\/");
+        String policyTopic = policyTopicSplit[policyTopicSplit.length - 1];
+        CambriaBatchingPublisher cambriaBatchingPublisher;
+        try {
+
+            cambriaBatchingPublisher = dmaapUtils.buildPublisher(configuration, policyTopic);
+
+            NotificationProducer notificationProducer = new NotificationProducer(cambriaBatchingPublisher);
+            notificationProducer.sendNotification(msg);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+}
