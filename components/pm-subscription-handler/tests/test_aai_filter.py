6462ff03f47a176fe6ebe6f1730a70b7f871db2f
egernug <gerard.nugent@est.tech>
2020-08-19 14:57:36 +0100

    Filter Enhancement
    Issue-ID: DCAEGEN2-2151
    
    Signed-off-by: egernug <gerard.nugent@est.tech>
    Change-Id: Ib6c207ac08a54dea06346596ff11c980fa4cf5e8

---
+++ components/pm-subscription-handler/tests/test_aai_filter.py
@@ -0,0 +1,101 @@
+from unittest import TestCase
+
+from mod.network_function import NetworkFunctionFilter
+
+
+class FilterTest(TestCase):
+    def test_filter_false_on_xnf(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+                "^pnf.*",
+                "^vnf.*"
+            ],
+            "modelInvariantUUIDs": [
+            ],
+            "modelVersionIDs": [
+            ]
+         })
+        self.assertFalse(nf_filter.is_nf_in_filter('PNF-33', '7129e420-d396-4efb-af02-6b83499b12f8', 'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
+                                                                 'Active'))
+
+    def test_filter_true_on_xnf(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+                "^pnf.*",
+                "^vnf.*"
+            ],
+            "modelInvariantUUIDs": [
+            ],
+            "modelVersionIDs": [
+            ]
+         })
+        self.assertTrue(nf_filter.is_nf_in_filter('pnf1', '7129e420-d396-4efb-af02-6b83499b12f8', 'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
+                                                                 'Active'))
+
+    def test_filter_true_on_modelInvariantUUIDs(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+            ],
+            "modelInvariantUUIDs": [
+                '5845y423-g654-6fju-po78-8n53154532k6',
+                '7129e420-d396-4efb-af02-6b83499b12f8'
+            ],
+            "modelVersionIDs": [
+            ]
+         })
+        self.assertTrue(nf_filter.is_nf_in_filter('pnf1', '7129e420-d396-4efb-af02-6b83499b12f8', 'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
+                                                                 'Active'))
+
+    def test_filter_false_on_modelInvariantUUIDs(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+            ],
+            "modelInvariantUUIDs": [
+                '5845y423-g654-6fju-po78-8n53154532k6',
+                '7129e420-d396-4efb-af02-6b83499b12f8'
+            ],
+            "modelVersionIDs": [
+            ]
+         })
+        self.assertFalse(nf_filter.is_nf_in_filter('pnf1', 'WrongModelInvariantUUID', 'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
+                                                                 'Active'))
+
+    def test_filter_true_on_modelVersionIDs(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+            ],
+            "modelInvariantUUIDs": [
+            ],
+            "modelVersionIDs": [
+                'e80a6ae3-cafd-4d24-850d-e14c084a5ca9'
+            ]
+         })
+        self.assertTrue(nf_filter.is_nf_in_filter('pnf1', '7129e420-d396-4efb-af02-6b83499b12f8', 'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
+                                                                 'Active'))
+
+    def test_filter_false_on_modelVersionIDs(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+            ],
+            "modelInvariantUUIDs": [
+            ],
+            "modelVersionIDs": [
+                'e80a6ae3-cafd-4d24-850d-e14c084a5ca9'
+            ]
+         })
+        self.assertFalse(nf_filter.is_nf_in_filter('pnf1', '7129e420-d396-4efb-af02-6b83499b12f8', 'WrongModelVersionID',
+                                                                 'Active'))
+
+    def test_filter_false_on_OrchestrationStatus(self):
+        nf_filter = NetworkFunctionFilter(**{
+            "nfNames":[
+                "^pnf.*",
+                "^vnf.*"
+            ],
+            "modelInvariantUUIDs": [
+            ],
+            "modelVersionIDs": [
+            ]
+         })
+        self.assertFalse(nf_filter.is_nf_in_filter('pnf1', '7129e420-d396-4efb-af02-6b83499b12f8', 'WrongModelVersionID',
+                                                                 'Inventoried'))
