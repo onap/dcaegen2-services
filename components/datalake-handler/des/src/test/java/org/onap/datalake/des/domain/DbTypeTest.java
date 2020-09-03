29b89154df062c6e26d665764f8e8e3ad5317836
Kai <lukai@chinamobile.com>
2020-09-03 16:36:54 +0800

    data extraction service
    Issue-ID: DCAEGEN2-2258
    Signed-off-by: Kai Lu <lukai@chinamobile.com>
    Change-Id: I9089acd786dfcf4d9b0c77e64f5deca3b7c4e6f6

---
+++ components/datalake-handler/des/src/test/java/org/onap/datalake/des/domain/DbTypeTest.java
@@ -0,0 +1,62 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : DataLake
+ * ================================================================================
+ * Copyright 2020 China Mobile
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.datalake.des.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Test Data Type.
+ *
+ * @author Kai Lu
+ */
+public class DbTypeTest {
+
+    @Test
+    public void test() {
+        DbType dbType = new DbType("ES","Elasticsearch");
+
+        dbType.setTool(false); 
+
+        assertNotNull(dbType.toString());
+        assertEquals(dbType, dbType);
+        assertNotEquals(dbType, null);
+        assertNotEquals(dbType, "ES");
+
+        DbType dbType2 = new DbType("MONGO", "MongoDB");
+        assertNotEquals(dbType, dbType2);
+        assertNotNull(dbType.hashCode());
+
+        assertEquals("MongoDB", dbType2.getName());
+        dbType2.setName(null);
+        dbType2.setDefaultPort(1);
+        assertTrue(1 == dbType2.getDefaultPort());
+
+        dbType2.setDbs(null);
+        assertNull(dbType2.getDbs());
+    }
+
+}
