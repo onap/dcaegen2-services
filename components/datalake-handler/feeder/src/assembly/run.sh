e5c611646a06c8fa8f0b61fda27b22333f819b72
Niranjana <niranjana.y60@wipro.com>
2021-02-24 14:19:20 +0000

    Use non-root user to access datalake-feeder database
    Issue-ID: DCAEGEN2-2329
    Signed-off-by: Niranjana <niranjana.y60@wipro.com>
    Change-Id: I26da297dcc4563a25d3cd6f558cc92627b17647b

--- components/datalake-handler/feeder/src/assembly/run.sh
+++ components/datalake-handler/feeder/src/assembly/run.sh
@@ -2,14 +2,21 @@
 
 echo "start init db ..."
 
-/bin/run-parts /home/datalake/db_init
+export PGPASSWORD=$PG_PASSWORD
+
+sh db_init/11_create-table
+sh db_init/20_db-initdb
 
 echo "finish init db"
 
 cmd=`find . -regex  '\./feeder-[0-9]+\.[0-9]+\.[0-9]+[-SNAPSHOT]+\-exec.jar'`
 cmd1=`find . -regex '\./feeder-[0-9]+\.[0-9]+\.[0-9]+\-exec.jar'`
+cmd=`find . -name feeder*-exec.jar`
 if [ -n "$cmd" ]; then
     java -jar $cmd
 elif [ -n "$cmd1" ]; then
     java -jar $cmd1
+else
+    echo "STRING is empty"
+    sleep 10000
 fi
