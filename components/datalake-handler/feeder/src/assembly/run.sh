4f438a4c4c753e3cd49e4dbaa68ee7f72628b548
Niranjana <niranjana.y60@wipro.com>
2021-02-24 14:19:20 +0000

    Use non-root user to access datalake-feeder database and update the base image to align with ONAP
    Issue-ID: DCAEGEN2-2329
    Issue-ID: DCAEGEN2-2420
    Signed-off-by: Niranjana <niranjana.y60@wipro.com>
    Change-Id: I26da297dcc4563a25d3cd6f558cc92627b17647b

--- components/datalake-handler/feeder/src/assembly/run.sh
+++ components/datalake-handler/feeder/src/assembly/run.sh
@@ -2,14 +2,20 @@
 
 echo "start init db ..."
 
-/bin/run-parts /home/datalake/db_init
+export PGPASSWORD=$PG_PASSWORD
+
+sh db_init/11_create-database
+sh db_init/20_db-initdb
 
 echo "finish init db"
 
-cmd=`find . -regex  '\./feeder-[0-9]+\.[0-9]+\.[0-9]+[-SNAPSHOT]+\-exec.jar'`
+cmd=`find . -name feeder*-exec.jar`
 cmd1=`find . -regex '\./feeder-[0-9]+\.[0-9]+\.[0-9]+\-exec.jar'`
 if [ -n "$cmd" ]; then
     java -jar $cmd
 elif [ -n "$cmd1" ]; then
     java -jar $cmd1
+else
+    echo "STRING is empty"
+    sleep 10000
 fi
