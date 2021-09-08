70de6a27b7722e3ed02d8e8a8c7933e053eacabb
SagarS <sagar.shetty@est.tech>
2021-09-08 14:46:32 +0100

    [DCAEGEN2] PMSH Create Subscription public API
    Issue-ID: DCAEGEN2-2819
    Change-Id: I80636be25dc4f7b1c5ce7470c7a38c010cb339a1
    Signed-off-by: SagarS <sagar.shetty@est.tech>

---
+++ components/pm-subscription-handler/tests/services/test_measurement_group_service.py
@@ -0,0 +1,113 @@
+# ============LICENSE_START===================================================
+#  Copyright (C) 2021 Nordix Foundation.
+# ============================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=====================================================
+
+import json
+import os
+from unittest.mock import patch
+from mod.network_function import NetworkFunction
+from mod.pmsh_config import AppConfig
+from mod import db
+from tests.base_setup import BaseClassSetup
+from mod.api.services import measurement_group_service
+from mod.api.db_models import MeasurementGroupModel, NfMeasureGroupRelationalModel
+from mod.subscription import SubNfState
+
+
+class MeasurementGroupServiceTestCase(BaseClassSetup):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def setUp(self):
+        super().setUp()
+        with open(os.path.join(os.path.dirname(__file__),
+                               '../data/create_subscription_request.json'), 'r') as data:
+            self.subscription_request = data.read()
+        with open(os.path.join(os.path.dirname(__file__), '../data/aai_xnfs.json'), 'r') as data:
+            self.aai_response_data = data.read()
+        with open(os.path.join(os.path.dirname(__file__), '../data/aai_model_info.json'),
+                  'r') as data:
+            self.good_model_info = data.read()
+
+    def tearDown(self):
+        super().tearDown()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    @patch.object(AppConfig, 'publish_to_topic')
+    def test_publish_measurement_group(self, mock_mr):
+        super().setUpAppConf()
+        nf_1 = NetworkFunction(**{'nf_name': 'pnf_1',
+                                  'ipv4_address': '204.120.0.15',
+                                  'ipv6_address': '2001:db8:3333:4444:5555:6666:7777:8888',
+                                  'model_invariant_id': 'some_id',
+                                  'model_version_id': 'some_other_id'},
+                               sdnc_model_name='blah',
+                               sdnc_model_version=1.0, )
+        measurement_grp = MeasurementGroupModel('sub_publish',
+                                                'msg_publish', 'UNLOCKED',
+                                                15, 'pm.xml', [{"measurementType": "counter_a"}],
+                                                [{"DN": "string"}])
+        measurement_group_service.publish_measurement_group(
+            'sub_publish', measurement_grp, nf_1)
+        mock_mr.assert_called_once_with('policy_pm_publisher',
+                                        {'nfName': 'pnf_1',
+                                         'ipAddress': '2001:db8:3333:4444:5555:6666:7777:8888',
+                                         'blueprintName': 'blah',
+                                         'blueprintVersion': 1.0,
+                                         'policyName': 'pmsh-operational-policy',
+                                         'changeType': 'CREATE',
+                                         'closedLoopControlName': 'pmsh-control-loop',
+                                         'subscription':
+                                             {'administrativeState': 'UNLOCKED',
+                                              'subscriptionName': 'sub_publish',
+                                              'fileBasedGP': 15,
+                                              'fileLocation': 'pm.xml',
+                                              'measurementGroup':
+                                                  {'measurementGroupName': 'msg_publish',
+                                                   'measurementTypes':
+                                                       [{"measurementType": "counter_a"}],
+                                                   'managedObjectDNsBasic': [{"DN": "string"}]}}})
+
+    def test_save_measurement_group(self):
+        subscription = json.loads(self.subscription_request)['subscription']
+        mes_grp = subscription['measurementGroups'][0]['measurementGroup']
+        measurement_group_service.save_measurement_group(mes_grp, "ExtraPM-All-gNB-R2B")
+        db.session.commit()
+        measurement_grp = (MeasurementGroupModel.query.filter(
+            MeasurementGroupModel.measurement_group_name == mes_grp['measurementGroupName'],
+            MeasurementGroupModel.subscription_name == 'ExtraPM-All-gNB-R2B').one_or_none())
+        self.assertIsNotNone(measurement_grp)
+
+    def test_apply_nf_to_measgroup(self):
+        measurement_group_service.apply_nf_to_measgroup("pnf_test", "measure_grp_name")
+        db.session.commit()
+        measurement_grp_rel = (NfMeasureGroupRelationalModel.query.filter(
+            NfMeasureGroupRelationalModel.measurement_grp_name == 'measure_grp_name',
+            NfMeasureGroupRelationalModel.nf_name == 'pnf_test').one_or_none())
+        db.session.commit()
+        self.assertIsNotNone(measurement_grp_rel)
+        self.assertEqual(measurement_grp_rel.nf_measure_grp_status,
+                         SubNfState.PENDING_CREATE.value)
+
+    def create_test_subs(self, new_sub_name, new_msrmt_grp_name):
+        subscription = self.subscription_request.replace('ExtraPM-All-gNB-R2B', new_sub_name)
+        subscription = subscription.replace('msrmt_grp_name', new_msrmt_grp_name)
+        return subscription
--- components/pm-subscription-handler/tests/test_controller.py
+++ components/pm-subscription-handler/tests/test_controller.py
@@ -1,5 +1,5 @@
 # ============LICENSE_START===================================================
-#  Copyright (C) 2019-2020 Nordix Foundation.
+#  Copyright (C) 2019-2021 Nordix Foundation.
 # ============================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,14 +17,15 @@
 # ============LICENSE_END=====================================================
 import json
 import os
-from unittest.mock import patch
-
+from unittest.mock import patch, MagicMock
 import responses
 from requests import Session
-
 from mod import aai_client
-from mod.api.controller import status, get_all_sub_to_nf_relations
+from mod.api.controller import status, get_all_sub_to_nf_relations, post_subscription
 from tests.base_setup import BaseClassSetup
+from mod.api.db_models import SubscriptionModel, NfMeasureGroupRelationalModel
+from mod.subscription import SubNfState
+from mod.network_function import NetworkFunctionFilter
 
 
 class ControllerTestCase(BaseClassSetup):
@@ -35,10 +36,14 @@ class ControllerTestCase(BaseClassSetup):
 
     def setUp(self):
         super().setUp()
+        super().setUpAppConf()
         with open(os.path.join(os.path.dirname(__file__), 'data/aai_xnfs.json'), 'r') as data:
             self.aai_response_data = data.read()
         with open(os.path.join(os.path.dirname(__file__), 'data/aai_model_info.json'), 'r') as data:
             self.good_model_info = data.read()
+        with open(os.path.join(os.path.dirname(__file__),
+                               'data/create_subscription_request.json'), 'r') as data:
+            self.subscription_request = data.read()
 
     def tearDown(self):
         super().tearDown()
@@ -62,10 +67,65 @@ class ControllerTestCase(BaseClassSetup):
                       '7129e420-d396-4efb-af02-6b83499b12f8/model-vers/model-ver/'
                       'e80a6ae3-cafd-4d24-850d-e14c084a5ca9',
                       json=json.loads(self.good_model_info), status=200)
-        self.xnfs = aai_client.get_pmsh_nfs_from_aai(self.app_conf)
+        self.xnfs = aai_client.get_pmsh_nfs_from_aai(self.app_conf, self.app_conf.nf_filter)
         sub_model = self.app_conf.subscription.get()
         for nf in self.xnfs:
             self.app_conf.subscription.add_network_function_to_subscription(nf, sub_model)
         all_subs = get_all_sub_to_nf_relations()
         self.assertEqual(len(all_subs[0]['network_functions']), 3)
         self.assertEqual(all_subs[0]['subscription_name'], 'ExtraPM-All-gNB-R2B')
+
+    def create_test_subs(self, new_sub_name, new_msrmt_grp_name):
+        subscription = self.subscription_request.replace('ExtraPM-All-gNB-R2B', new_sub_name)
+        subscription = subscription.replace('msrmt_grp_name', new_msrmt_grp_name)
+        return subscription
+
+    @patch('mod.api.services.subscription_service.save_nf_filter', MagicMock(return_value=None))
+    @patch('mod.pmsh_config.AppConfig.publish_to_topic', MagicMock(return_value=None))
+    @patch.object(aai_client, '_get_all_aai_nf_data')
+    @patch.object(aai_client, 'get_aai_model_data')
+    @patch.object(NetworkFunctionFilter, 'get_network_function_filter')
+    def test_post_subscription(self, mock_filter_call, mock_model_aai, mock_aai):
+        mock_aai.return_value = json.loads(self.aai_response_data)
+        mock_model_aai.return_value = json.loads(self.good_model_info)
+        subscription = self.create_test_subs('xtraPM-All-gNB-R2B-post', 'msrmt_grp_name-post')
+        subscription = json.loads(subscription)
+        mock_filter_call.return_value = NetworkFunctionFilter(
+            **subscription['subscription']["nfFilter"])
+        sub_name = subscription['subscription']['subscriptionName']
+        mes_grp = subscription['subscription']['measurementGroups'][0]['measurementGroup']
+        mes_grp_name = mes_grp['measurementGroupName']
+        response = post_subscription(subscription)
+        subscription = (SubscriptionModel.query.filter(
+            SubscriptionModel.subscription_name == sub_name).one_or_none())
+        self.assertIsNotNone(subscription)
+        msr_grp_nf_rel = (NfMeasureGroupRelationalModel.query.filter(
+            NfMeasureGroupRelationalModel.measurement_grp_name == mes_grp_name)).all()
+        for published_event in msr_grp_nf_rel:
+            self.assertEqual(published_event.nf_measure_grp_status,
+                             SubNfState.PENDING_CREATE.value)
+        self.assertEqual(response[1], 201)
+
+    def test_post_subscription_duplicate_sub(self):
+        # Posting the same subscription request stored in previous test to get duplicate response
+        response = post_subscription(json.loads(self.subscription_request))
+        self.assertEqual(response[1], 409)
+        self.assertEqual(response[0], 'subscription Name: ExtraPM-All-gNB-R2B already exists.')
+
+    def test_post_subscription_invalid_filter(self):
+        subscription = self.create_test_subs('xtraPM-All-gNB-R2B-invalid', 'msrmt_grp_name-invalid')
+        subscription = json.loads(subscription)
+        subscription['subscription']['nfFilter']['nfNames'] = []
+        subscription['subscription']['nfFilter']['modelInvariantIDs'] = []
+        subscription['subscription']['nfFilter']['modelVersionIDs'] = []
+        subscription['subscription']['nfFilter']['modelNames'] = []
+        response = post_subscription(subscription)
+        self.assertEqual(response[1], 400)
+        self.assertEqual(response[0], 'At least one filter within nfFilter must not be empty')
+
+    def test_post_subscription_missing(self):
+        subscription = json.loads(self.subscription_request)
+        subscription['subscription']['subscriptionName'] = ''
+        response = post_subscription(subscription)
+        self.assertEqual(response[1], 400)
+        self.assertEqual(response[0], 'No value provided in subscription name')
