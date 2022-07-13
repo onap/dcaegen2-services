f019d46e66fa2ccc6b6ca48500aa33ad56b2c9e1
malar <malarvizhi.44@wipro.com>
2022-07-13 05:41:41 +0000

    Enhancements to KPI Computation MS for Kohn Release
    - Changes to support two types of PM XML schema
    - Throw an exception for Ratio and SumRatio computation when
      minimum operand size is not satisfied
    
    Issue-ID: DCAEGEN2-3193
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: Ie9672bb11a5e98aa681bd1c7c7db59ea2be46112

--- components/kpi-computation-ms/src/test/java/org/onap/dcaegen2/kpi/computation/KpiComputationTest.java
+++ components/kpi-computation-ms/src/test/java/org/onap/dcaegen2/kpi/computation/KpiComputationTest.java
@@ -2,6 +2,7 @@
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2021 China Mobile.
  *  Copyright (C) 2022 Deutsche Telekom AG. All rights reserved.
+ *  Copyright (C) 2022 Wipro Limited. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,9 +41,11 @@ public class KpiComputationTest {
     private static final String KPI_CONFIG_FILE = "kpi/kpi_config.json";
     private static final String VES_MESSAGE_FILE = "kpi/ves_message.json";
     private static final String KPI_CONFIG_RATIO_FILE = "kpi/kpi_config_ratio.json";
+    private static final String KPI_CONFIG_SLICING_RATIO_FILE = "kpi/kpi_config_slicing.json";
     private static final String KPI_CONFIG_SUMRATIO_FILE = "kpi/kpi_config_sumratio.json";
     private static final String VES_MESSAGE_EMPTY_FILE = "kpi/ves_message_empty.json";
     private static final String VES_MESSAGE_NULL_FILE = "kpi/ves_message_null.json";
+    private static final String VES_MESSAGE_SLICING_FILE = "kpi/ves_message_slicing.json";
     private static final String VES_MESSAGE_EVENTNAME_FILE = "kpi/ves_message_eventname.json";
 
     @Test
@@ -72,6 +75,18 @@ public class KpiComputationTest {
         VesEvent vesEvent = vesList.get(0);
         assertEquals(vesEvent.getEvent().getPerf3gppFields().getMeasDataCollection().getMeasInfoList().get(0)
                  .getMeasValuesList().get(0).getMeasResults().get(0).getSvalue(), "50");
+    }
+
+    @Test
+    public void testKpiComputationSlicingRatio() {
+        String strKpiConfigRatio = FileUtils.getFileContents(KPI_CONFIG_SLICING_RATIO_FILE);
+        String vesMessage = FileUtils.getFileContents(VES_MESSAGE_SLICING_FILE);
+        Configuration config = mock(Configuration.class);
+        when(config.getKpiConfig()).thenReturn(strKpiConfigRatio);
+        List<VesEvent> vesList = new KpiComputation().checkAndDoComputation(vesMessage, config);
+        VesEvent vesEvent = vesList.get(0);
+        assertEquals(vesEvent.getEvent().getPerf3gppFields().getMeasDataCollection().getMeasInfoList().get(0)
+                     .getMeasValuesList().get(0).getMeasResults().get(0).getSvalue(), "158");
     }
 
     @Test
