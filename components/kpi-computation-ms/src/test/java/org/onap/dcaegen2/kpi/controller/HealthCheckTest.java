824cb53a13996844575a8f81907222ef9c2193fb
Ahila <ahila.pandaram@wipro.com>
2022-08-04 16:14:16 +0530

    KPI-Computation-MS Code coverage improvement
    Issue-ID: DCAEGEN2-3162
    
    Signed-off-by: Ahila <ahila.pandaram@wipro.com>
    Change-Id: I26ec8c7cd591c059395c0cedd471643e3e87c114

---
+++ components/kpi-computation-ms/src/test/java/org/onap/dcaegen2/kpi/controller/HealthCheckTest.java
@@ -0,0 +1,41 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Wipro Limited.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.kpi.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class HealthCheckTest {
+    @Spy
+    HealthCheck healthCheck;
+	@Test
+	public void testHealthCheck(){
+		ResponseEntity<HttpStatus> status = healthCheck.healthCheck();
+		assertEquals(HttpStatus.OK, status.getStatusCode());
+	}
+}
