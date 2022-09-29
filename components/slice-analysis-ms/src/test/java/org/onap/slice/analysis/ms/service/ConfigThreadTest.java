95bf574077bff8fac0c65eb857d48fe13a762069
zhuguanyu <zhuguanyu5@huawei.com>
2022-09-29 11:53:01 +0800

    Test cases for ConfigThread and CCVPNPmDatastore
    Issue-ID: DCAEGEN2-3275
    Signed-off-by: zhuguanyu <zhuguanyu5@huawei.com>
    Change-Id: I61cb9f4bc9f2a4b2d4a5843fa0b292e72cf80230

---
+++ components/slice-analysis-ms/src/test/java/org/onap/slice/analysis/ms/service/ConfigThreadTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ *  ============LICENSE_START=======================================================
+ *  slice-analysis-ms
+ *  ================================================================================
+ *  Copyright (C) 2022 CTC, Inc.
+ *  ==============================================================================
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *     ============LICENSE_END=========================================================
+ *
+ *******************************************************************************/
+
+package org.onap.slice.analysis.ms.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.onap.slice.analysis.ms.models.ConfigPolicy;
+import org.onap.slice.analysis.ms.service.ccvpn.CCVPNPmDatastore;
+import org.onap.slice.analysis.ms.utils.BeanUtil;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.powermock.api.mockito.PowerMockito.doThrow;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*"})
+@PowerMockRunnerDelegate(SpringRunner.class)
+@PrepareForTest({ BeanUtil.class, ConfigPolicy.class })
+@SpringBootTest(classes = ConfigThreadTest.class)
+public class ConfigThreadTest {
+
+    private ConfigThread configThread;
+
+    @Mock
+    private CCVPNPmDatastore ccvpnPmDatastore;
+
+    @Before
+    public void before() throws IllegalAccessException {
+        PowerMockito.mockStatic(BeanUtil.class);
+        when(BeanUtil.getBean(CCVPNPmDatastore.class)).thenReturn(ccvpnPmDatastore);
+        configThread = PowerMockito.spy(new ConfigThread());
+        MemberModifier.field(ConfigThread.class, "ccvpnPmDatastore")
+            .set(configThread , ccvpnPmDatastore);
+    }
+
+    @Test
+    public void run1Test() {
+        ConfigPolicy configPolicy = mock(ConfigPolicy.class);
+        PowerMockito.mockStatic(ConfigPolicy.class);
+        PowerMockito.when(ConfigPolicy.getInstance()).thenReturn(configPolicy);
+        doThrow(new RuntimeException()).when(configPolicy).getConfig();
+        configThread.run();
+        Mockito.verify(configThread, Mockito.atLeastOnce()).run();
+    }
+
+    @Test
+    public void run2Test() {
+        ConfigPolicy configPolicy = mock(ConfigPolicy.class);
+        PowerMockito.mockStatic(ConfigPolicy.class);
+        PowerMockito.when(ConfigPolicy.getInstance()).thenReturn(configPolicy);
+        Map<String, Object> config = mock(HashMap.class);
+        when(configPolicy.getConfig()).thenReturn(config);
+        when(config.containsKey("cllId")).thenReturn(true);
+
+        when(config.containsKey("cllId")).thenReturn(true).thenThrow(new RuntimeException());
+        configThread.run();
+        Mockito.verify(configThread, Mockito.atLeastOnce()).run();
+    }
+
+    @Test
+    public void run3Test() {
+        ConfigPolicy configPolicy = mock(ConfigPolicy.class);
+        PowerMockito.mockStatic(ConfigPolicy.class);
+        PowerMockito.when(ConfigPolicy.getInstance()).thenReturn(configPolicy);
+        Map<String, Object> config = mock(HashMap.class);
+        when(configPolicy.getConfig()).thenReturn(config);
+        when(config.containsKey("closedLoopStatus")).thenReturn(true);
+
+        when(config.containsKey("closedLoopStatus")).thenReturn(true).thenThrow(new RuntimeException());
+        configThread.run();
+        Mockito.verify(configThread, Mockito.atLeastOnce()).run();
+    }
+
+    @Test
+    public void run4Test() {
+        ConfigPolicy configPolicy = mock(ConfigPolicy.class);
+        PowerMockito.mockStatic(ConfigPolicy.class);
+        PowerMockito.when(ConfigPolicy.getInstance()).thenReturn(configPolicy);
+        Map<String, Object> config = mock(HashMap.class);
+        when(configPolicy.getConfig()).thenReturn(config);
+        when(config.containsKey("originalBw")).thenReturn(true);
+
+        when(config.containsKey("originalBw")).thenReturn(true).thenThrow(new RuntimeException());
+        configThread.run();
+        Mockito.verify(configThread, Mockito.atLeastOnce()).run();
+    }
+
+}
